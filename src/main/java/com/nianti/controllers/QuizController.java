6ae467552337d88403fcef7fc15ef81a86ca034a
Robin Weaver <strwtch13@gmail.com>
2024-09-05 13:46:00 -0700

    Created quiz.html, added clickable links to homepage

--- src/main/java/com/nianti/controllers/QuizController.java
+++ src/main/java/com/nianti/controllers/QuizController.java
@@ -3,10 +3,12 @@ package com.nianti.controllers;
 import com.nianti.models.Quiz;
 import com.nianti.services.QuizDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
 
+@Controller
 public class QuizController {
 
     @Autowired
